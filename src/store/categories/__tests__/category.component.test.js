73cf7392b2260332d7e6667ec61306ff53fafaf6
Khaled Abdulhamid <khaledraedabdulhamed101@hotmail.com>
2023-09-05 20:50:47 -0400

    adding the Category Reducer tests cases for success faild and start reducers

---
+++ src/store/categories/__tests__/category.component.test.js
@@ -0,0 +1,59 @@
+import {
+    categoriesReducer,
+    CATEGORIES_INITIAL_STATE
+} from '../category.reducer';
+
+import {
+fetchCategoriesStart,
+FetchCategoriesFailed,
+fetchCategoriesSuccess,
+fetchCategoriesFailed,
+} from '../category.action'
+
+describe('Category Reducer tests', () => {
+   
+    test('fetchCategoriesStart', () => {
+        const expectedState = {
+            ...CATEGORIES_INITIAL_STATE,
+            isLoading: true,
+        }
+        expect(categoriesReducer(CATEGORIES_INITIAL_STATE, fetchCategoriesStart())).toEqual(expectedState)
+    })
+
+    test('fetchCategoriesSuccess', () => {
+        const mockData = [
+            {
+                title: 'mens',
+                imageUrl: 'test',
+                items: [
+                    {id: 1, name: 'product 1'},
+                    {id: 1, name: 'product 1'}
+                ]
+            },
+            {
+                title: 'women',
+                imageUrl: 'test 2',
+                items: [
+                    {id: 3, name: 'product 3'},
+                    {id: 4, name: 'product 4'}
+                ]
+            }
+        ]
+        const expectedState = {
+            ...CATEGORIES_INITIAL_STATE,
+            isLoading: false,
+            categories: mockData
+        }
+        expect(categoriesReducer(CATEGORIES_INITIAL_STATE, fetchCategoriesSuccess(mockData))).toEqual(expectedState)
+    })
+
+    test('fetchCategoriesFaild', () => {
+        const mockError = new Error('Error Fetching Categories');
+        const expectedState = {
+            ...CATEGORIES_INITIAL_STATE,
+            isLoading: false,
+            error: mockError
+        }
+        expect(categoriesReducer(CATEGORIES_INITIAL_STATE, fetchCategoriesFailed(mockError))).toEqual(expectedState)
+    })
+})
